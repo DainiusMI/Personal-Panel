9e82d0f70b5486af205e5e50f342d509fa035747
DainiusMI <mental.def@gmail.com>
2023-02-03 18:18:17 +0200

    save note

--- src/assets/components/ToDo.jsx
+++ src/assets/components/ToDo.jsx
@@ -6,46 +6,30 @@ export default function ToDo() {
     const [toDoData, setToDoData] = useState([
         {
             text: "First To Do is to check text wrap",
-            isActive: false
+            isFocused: false,
+            input_active: false
         },
         {
             text: "First To Do is to check text wrap",
-            isActive: false
+            isFocused: false,
+            input_active: false
         }
     ])
 
-    const [addNote, setAddNote] = useState({
-        text: "",
-        input_hidden: true
-    })
 
-    const handleInput = (e) => {
-        setAddNote(prevState => ({
-            ...prevState,
-            text: e.target.value
-        }))
-    }
-
-    const addToDo = () => {
-        if (addNote.input_hidden) {
-            setAddNote(prevState => ({
-                ...prevState,
-                input_hidden: false
-            }))
-        }
-        else {
-            setToDoData(prevData => ([
-                ...prevData,
-                {
-                    text: addNote.text,
-                    isActive: false
-                }
-            ]))
-            setAddNote({
+    
+    const addNote = () => {
+        const notActive = toDoData.map(note => {
+            return {...note, isFocused: false, input_active: false}
+        })
+        setToDoData([
+            ...notActive,
+            {
                 text: "",
-                input_hidden: true
-            })
-        }
+                isFocused: true,
+                input_active: true
+            }
+        ])
     }
 
     const handleColumns = () => {
@@ -61,13 +45,14 @@ export default function ToDo() {
             }
         }
     }
-
+    console.log(toDoData)
     return (
         <div className="todo">
-            <div className="add_note">
-                <i 
-                    className="fa-solid fa-plus"
-                />
+            <div 
+                className="add_note" 
+                onClick={addNote}
+            >
+                <i className="fa-solid fa-plus"/>
                 <p className="todo__add__dummy__text"> Add</p>
             </div>
 
@@ -98,16 +83,59 @@ export default function ToDo() {
 
 function Note({id, item, toDoData, setToDoData}) {
 
-    const [focusedToDo, setFocusedToDo] = useState()
+    const [noteData, setNoteData] = useState(toDoData[id])
+    useEffect(() => {
+        setNoteData(toDoData[id])
+    }, [toDoData[id]])
+
+    const handleInput = (e) => {
+        setNoteData(prevState => ({
+            ...prevState,
+            text: e.target.value
+        }))
+    }
+    const saveNote = () => {
+        const data = [...toDoData]
+        data[id] = {
+            text: noteData.text,
+            isFocused: true,
+            input_active: false
+        }
+        setToDoData(data)
+    }
+    const focusNote = () = {
+        
+    }
 
     return (
         <li className="note">
             <p className="note__id">#{id+1}</p>
+            {
+                toDoData[id].input_active && 
+                <textarea  
+                    type="text"
+                    className="note__textarea"
+                    autoFocus
+                    value={noteData.text}
+                    onChange={handleInput}
+                />
+            }
             <p className="note__text">{item.text}</p>
-            <div className="note__actions">
-                <i className="fa-solid fa-pen-to-square"/>
-                <i className="fa-solid fa-trash"/>
-            </div>
+            {
+                toDoData[id].isFocused && 
+                <div className="note__actions">
+                    <i className="fa-solid fa-pen-to-square"/>
+                    <i className="fa-solid fa-trash"/>
+                </div>
+            }
+            {
+                toDoData[id].input_active && 
+                <i 
+                    className="save fa-solid fa-check"
+                    data-note={id}
+                    onClick={saveNote}
+                />
+            }
         </li>
     )
 }
